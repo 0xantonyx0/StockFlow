a717299034410de5c0cbce9db0f773f9140fd64d
Antony.exe <93298989+0xantonyx0@users.noreply.github.com>
2026-06-07 14:44:54 -0300

    Código-fonte do projeto.

---
+++ Project/Entregas.xaml.cs
@@ -0,0 +1,32 @@
+﻿using StockFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockFlow
+{
+    /// <summary>
+    /// Interação lógica para Entregas.xam
+    /// </summary>
+    public partial class Entregas : Page
+    {
+        public Entregas()
+        {
+            InitializeComponent();
+            EntregasCls entregasCls = new EntregasCls();
+
+            TextRange rctb = new TextRange(rtbdados.Document.ContentStart, rtbdados.Document.ContentEnd);
+
+            rctb.Text = entregasCls.CarregarDados();
+        }
+    }
+}
